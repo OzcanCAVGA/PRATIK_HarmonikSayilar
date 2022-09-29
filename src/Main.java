f36e9e12cd8b6145b2395e74eeb483239e800178
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 19:51:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+/*
+Java ile girilen sayının harmonik serisini bulan program yazacağız.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner yazici = new Scanner(System.in);
+        double sayi;
+        double toplam = 0;
+        sayi = yazici.nextInt();
+        for (int i = 1; i <= sayi; i++) {
+            toplam += 1 / (double)i;
+        }
+        System.out.println(toplam);
+
     }
 }
